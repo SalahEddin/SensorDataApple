2a8e68383604a43d2691533ef57d755b4dacbb3f
salah eddin alshaal <salshaal@gmail.com>
2015-11-20 22:03:47 -0800

    Initial Commit

--- SupremeCommander WatchKit Extension/InterfaceController.swift
+++ SupremeCommander WatchKit Extension/InterfaceController.swift
@@ -8,10 +8,18 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
+import HealthKit
+import CoreMotion
 
-
-class InterfaceController: WKInterfaceController {
-
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    var session: WCSession!
+    let motionManager = CMMotionManager()
+    
+    @IBOutlet var mMessageLabel: WKInterfaceLabel!
+    @IBOutlet var xLabel: WKInterfaceLabel!
+    @IBOutlet var yLabel: WKInterfaceLabel!
+    @IBOutlet var zLabel: WKInterfaceLabel!
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
@@ -21,11 +29,55 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        if(WCSession.isSupported()){
+            self.session = WCSession.defaultSession()
+            self.session.delegate = self
+            self.session.activateSession()
+            session.sendMessage(["b":"Session started"], replyHandler: nil, errorHandler: nil)
+        }
+        
+        if (motionManager.accelerometerAvailable == true) {
+            let handler:CMAccelerometerHandler = {(data: CMAccelerometerData?, error: NSError?) -> Void in
+                self.xLabel.setText(String(format: "%.2f", data!.acceleration.x))
+                self.yLabel.setText(String(format: "%.2f", data!.acceleration.y))
+                self.zLabel.setText(String(format: "%.2f", data!.acceleration.z))
+                self.session.sendMessage(["x":String(format: "%.2f", data!.acceleration.x)], replyHandler: nil, errorHandler: nil)
+                self.session.sendMessage(["y":String(format: "%.2f", data!.acceleration.y)], replyHandler: nil, errorHandler: nil)
+                self.session.sendMessage(["z":String(format: "%.2f", data!.acceleration.z)], replyHandler: nil, errorHandler: nil)
+            }
+            motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: handler)
+        }
+        else {
+            self.xLabel.setText("not available")
+            self.yLabel.setText("not available")
+            self.zLabel.setText("not available")
+        }
+
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+        // stop updating accel values
+        motionManager.stopAccelerometerUpdates()
     }
-
+    
+    @IBAction func sendMessage() {
+        if(WCSession.isSupported()){
+            session.sendMessage(["b":"goodBye"], replyHandler: nil, errorHandler: nil)
+        }
+    }
+    
+    @IBAction func sendHeartRate() {
+        // create an instance of CHeart
+        if(WCSession.isSupported()){
+            session.sendMessage(["b":"goodBye"], replyHandler: nil, errorHandler: nil)
+        }
+    }
+    
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
+        //recieving message from iphone
+        self.mMessageLabel.setText(message["a"]! as? String)
+    }
 }
--- SupremeCommander/ViewController.swift
+++ SupremeCommander/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WCSessionDelegate {
+    var session: WCSession!
+    @IBOutlet weak var mResultLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if(WCSession.isSupported()){
+            self.session = WCSession.defaultSession()
+            self.session.delegate = self
+            self.session.activateSession()
+        }
     }
 
+    @IBAction func sendMessageToWatch(sender: AnyObject) {
+        //send message to watch
+        session.sendMessage(["a":"hello"], replyHandler: nil, errorHandler: nil)
+    }
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
+        //recieve messages from watch
+        self.mResultLabel.text = message["b"]! as? String
+        self.xLabel.text = message["x"]! as? String
+        self.yLabel.text = message["y"]! as? String
+        self.zLabel.text = message["z"]! as? String
+        dispatch_async(dispatch_get_main_queue(), {
+            self.mResultLabel.text = message["b"]! as? String
+        })
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
