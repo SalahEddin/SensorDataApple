9a4165427e523913a44182348369cf3a199a0635
salah eddin alshaal <salshaal@gmail.com>
2015-11-29 11:49:31 +0200

    Initial Commit

---
+++ Fiddle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fiddle
+//
+//  Created by Salah Eddin Alshaal on 29/11/15.
+//  Copyright © 2015 Salah Eddin Alshaal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
